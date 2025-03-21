23851953c92d53aebb7adec359ef95dc0008f9ef
Bulagorn Sasanthei <bulagorn4811@gmail.com>
2025-03-22 00:15:29 +0700

    rename from index to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+
 import transactionRoutes from './infrastructure/routes/transaction.route';
-import userRoutes from './infrastructure/routes/user.route';
+import authRoutes from './infrastructure/routes/auth.route';
+import accountRoutes from './infrastructure/routes/account.route';
 import { myDataSource } from './infrastructure/config/typeorm.config';
 import { accessLogger } from './infrastructure/middlewares/logger.middleware';
 import { errorHandler } from './infrastructure/middlewares/error.middleware';
 
 dotenv.config();
 
-const app = express();
+export const app = express();
 
 app.use(accessLogger);
 app.use(express.json());
@@ -18,16 +20,17 @@ app.get('/healthcheck', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello World' });
 });
 
+app.use('/', transactionRoutes);
+app.use('/auth', authRoutes);
+app.use('/account', accountRoutes);
+
+// Global error handler (last middleware)
+app.use(errorHandler);
+
 myDataSource
   .initialize()
   .then(() => {
     console.log('Data Source has been initialized!');
-
-    app.use('/', transactionRoutes);
-    app.use('/user', userRoutes);
-
-    // Global error handler (last middleware)
-    app.use(errorHandler);
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
